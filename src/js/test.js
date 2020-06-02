abf5175e7ae53398b3387af1c800ac2874f0da0e
ev3rse <ev3rse@yandex.ru>
2020-06-02 12:16:53 +0700

    Added gulp starter.

---
+++ src/js/test.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const materials = [
+    'Hydrogen',
+    'Helium',
+    'Lithium',
+    'Beryllium',
+  ];
+
+  console.log(materials);
+});
